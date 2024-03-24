021b400f000d1bf50d938fea4209c714c16be4ce
Jason Liquorish <jason@liquori.sh>
2024-03-24 20:24:44 +0000

    Chapter 10 (WIP)

--- src/configuration.rs
+++ src/configuration.rs
@@ -8,6 +8,7 @@ pub struct Settings {
     pub database: DatabaseSettings,
     pub application: ApplicationSettings,
     pub email_client: EmailClientSettings,
+    pub redis_uri: Secret<String>,
 }
 
 #[derive(Clone, serde::Deserialize)]
@@ -34,6 +35,7 @@ pub struct ApplicationSettings {
     pub port: u16,
     pub host: String,
     pub base_url: String,
+    pub hmac_secret: Secret<String>,
 }
 
 #[derive(Clone, serde::Deserialize)]
